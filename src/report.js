bedc707b412f651fac9c471713eccdfeb7f0d4d7
Patrick Surrey <patrick.surrey@leanix.net>
2017-11-22 10:53:49 +0100

    Adds average calculation

--- src/report.js
+++ src/report.js
@@ -25,12 +25,31 @@ export class Report {
   }
 
   render(data) {
-    const html = data.map(fs => `<p>${fs.type}: ${fs.displayName}<div>${this.renderRelations(fs)}</div></p>`).join('');
+    let completion = this.computeAverageCompletion(data);
+    completion = completion.sort((a,b) => b.y - a.y);
+
+    const html = completion.map(c => `<p>${c.name}: ${c.y}</p>`).join('');
     $('#report').html(html);
   }
 
-  renderRelations(fs) {
-    return fs.relUserGroupToApplication.edges.map(e => e.node.factSheet.displayName).join('<br>');
+  computeAverageCompletion(data) {
+    return data.map((userGroup) => {
+      return {
+        name: userGroup.displayName,
+        y: this.computeUserGroupCompletion(userGroup)
+      };
+    });
+  }
+
+  computeUserGroupCompletion(userGroup) {
+    let completion = 0;
+    if(userGroup.relUserGroupToApplication.edges.length > 0) {
+      completion = userGroup.relUserGroupToApplication.edges.reduce((sum, val) => {
+        return sum + val.node.factSheet.completion.percentage;
+      }, 0.0);
+      completion = completion / userGroup.relUserGroupToApplication.edges.length;
+    }
+    return completion;
   }
 
 }

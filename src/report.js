338dcd3aa0f6b983ac26be6486635fc004148130
Patrick Surrey <patrick.surrey@leanix.net>
2017-11-22 10:52:56 +0100

    Fetch related Fact Sheets

--- src/report.js
+++ src/report.js
@@ -1,3 +1,13 @@
+const RELATION_FRAGMENT = `
+  relUserGroupToApplication {
+    edges {
+      node {
+        factSheet { displayName completion { percentage } }
+      }
+    }
+  }
+`;
+
 export class Report {
 
   constructor(setup) {
@@ -7,15 +17,20 @@ export class Report {
   createConfig() {
     return {
       facets: [{
-        attributes: ['id', 'type'],
+        fixedFactSheetType: 'UserGroup',
+        attributes: ['id', 'type', 'displayName', RELATION_FRAGMENT],
         callback: this.render.bind(this)
       }]
     };
   }
 
   render(data) {
-    const html = data.map(fs => `<p>${fs.type}: ${fs.id}</p>`).join('');
+    const html = data.map(fs => `<p>${fs.type}: ${fs.displayName}<div>${this.renderRelations(fs)}</div></p>`).join('');
     $('#report').html(html);
+  }
+
+  renderRelations(fs) {
+    return fs.relUserGroupToApplication.edges.map(e => e.node.factSheet.displayName).join('<br>');
   }
 
 }
